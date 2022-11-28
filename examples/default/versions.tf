cbd61eca010e5617e36b34bb9a6bb6592df154fb
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-11-28 16:34:51 +0000

    Update versions.tf

--- examples/default/versions.tf
+++ examples/default/versions.tf
@@ -10,6 +10,7 @@ terraform {
     }
   }
   required_version = ">=1.2"
+  experiments      = [module_variable_optional_attrs]
 }
 
 ##############################################################################
