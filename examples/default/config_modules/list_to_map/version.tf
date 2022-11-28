1dec07ad7283fb8d8e6ee8af822f7117bae232dc
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2022-11-28 16:35:10 +0000

    Update version.tf

--- examples/default/config_modules/list_to_map/version.tf
+++ examples/default/config_modules/list_to_map/version.tf
@@ -4,6 +4,7 @@
 
 terraform {
   required_version = ">=1.2"
+  experiments      = [module_variable_optional_attrs]
 }
 
 ##############################################################################
