49cd677f697fbacb94fbb1c6ce159df6320d7153
Md Anam Raihan <md.anam.raihan17@gmail.com>
2023-01-25 21:41:50 +0530

    feat: add support for terraform >= 1.3 (#130)
    BREAKING CHANGE: module is no longer supported on terraform versions lower than 1.3

--- examples/default/config_modules/list_to_map/version.tf
+++ examples/default/config_modules/list_to_map/version.tf
@@ -4,7 +4,6 @@
 
 terraform {
   required_version = ">=1.2"
-  experiments      = [module_variable_optional_attrs]
 }
 
 ##############################################################################
